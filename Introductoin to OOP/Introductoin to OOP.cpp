b4c50428bcc007f6259c634a57786d649961ab82
trimmergrom@yandex.ru <trimmergrom@yandex.ru>
2021-12-11 19:56:14 +0300

    1

--- Introductoin to OOP/Introductoin to OOP.cpp
+++ Introductoin to OOP/Introductoin to OOP.cpp
@@ -4,6 +4,7 @@ class Point
 {
 	double x;
 	double y;
+	double c;
 public:
 	double get_x()const
 	{
@@ -13,6 +14,7 @@ public:
 	{
 		return y;
 	}
+	
 	void set_x(double x)
 	{
 		this -> x = x;
@@ -21,13 +23,44 @@ public:
 	{
 		this -> y = y;
 	}
-}A,B,C;
+	/*Point()
+	{
+		x = y = 0;
+		std::cout << " DefaultConstructor:\t" << this << std::endl;
+	}*/
+	Point(double x = 0, double y = 0)
+	{
+		this->x = x;
+		this->y = y;
+		std::cout << "Constructor:\t" << this << std::endl;
+	}
+	~ Point()
+	{
+		std::cout << "Destuctor:\t" << this << std::endl;
+	}
+	void Print()const
+	{
+		std::cout << "x = " << x /*<< std::endl*/;
+		std::cout << "\ty = " << y << std::endl;
+	}
+};
 #define struct_POINT
 int main()
 {
 	Point A;
-	A.set_x(2);
-	A.set_y(3);
+	double x, y;
+	A.Print();
+
+	Point B(22, 33);
+	B.Print();
+	Point C = 5;
+	C.Print();
+	Point D(0, 123);
+	D.Print();
+
+
+	//A.set_x(2);
+	//A.set_y(3);
 
 	std::cout << A.get_x() << "\t" << A.get_y() << std::endl;
 }
