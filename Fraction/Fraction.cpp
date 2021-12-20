d4a169e6475a39b6287501019c51e647c156aa1e
trimmergrom@yandex.ru <trimmergrom@yandex.ru>
2021-12-20 22:11:57 +0300

    Fraction_2

--- Fraction/Fraction.cpp
+++ Fraction/Fraction.cpp
@@ -4,15 +4,17 @@
 
 class Fraction
 {
-    //int A;
+    int A;
     int a;
     int b;
+
+    
     
 public:
-   /* int get_A()const
+    int get_A()const
     {
         return A;
-    }*/
+    }
     int get_a()const
     {
         return a;
@@ -21,10 +23,10 @@ public:
     {
         return b;
     }
-   /* void set_A(int a)
+   void set_A(int a)
     {
         this->A = A;
-    }*/
+    }
     void set_a(int a)
     {
         this->a = a;
@@ -44,13 +46,13 @@ public:
         this->b = b;
         //std::cout << "Constructor\t" << this << std::endl;
     }
-    /*Fraction(int A, int a, int b)
+    Fraction(int A, int a, int b)
     {
         this->A = A;
         this->a = a;
         this->b = b;
         std::cout << "Constructor_1\t" << this << std::endl;
-    }*/
+    }
     ~Fraction()
     {
         //std::cout << "Destructor\t" << this << std::endl;
@@ -62,8 +64,22 @@ public:
     }
     void print_1()const
     {
-        std::cout << "(" << a << " / " << b << ")" << std::endl;
+        std::cout << A << "(" << a << " / " << b << ")" << std::endl;
     }
+    Fraction& reduce()
+    {
+        int a_gcf = this->a; int b_gcf = this->b;
+        while (a_gcf != 0 && b_gcf != 0)
+        {
+            if (a_gcf > b_gcf) { a_gcf = a_gcf % b_gcf; }
+            else { b_gcf = b_gcf % a_gcf; }
+        }
+        int gcf = a_gcf + b_gcf;
+        this->a = this->a / gcf;
+        this->b = this->b / gcf;        
+        return  *this;
+    }
+    
     Fraction& operator+=(const Fraction& other)
     {
         this->a = this->a*other.b + other.a*this->b;
@@ -120,8 +136,8 @@ public:
 
 int main()
 {
-    Fraction A(1, 3);
-    Fraction E(1, 3);
+    Fraction A(148, 56);
+    Fraction E(25, 450);
     Fraction C(1, 3);
     Fraction B(2, 7);
     Fraction R(2, 7);
@@ -134,14 +150,17 @@ int main()
     //std::cout << "A + B = "; (A + B).print();
     //std::cout << "A += B = "; (A += B).print();
     //std::cout << "A - B = "; (E - R).print();
-    std::cout << "A -= B = "; (A -= B).print();
-    std::cout << "B - A = "; (R - E).print();
-    std::cout << "B -= A = "; (R -= E).print();
+    //std::cout << "A -= B = "; (A -= B).print();
+    //std::cout << "B - A = "; (R - E).print();
+    //std::cout << "B -= A = "; (R -= E).print();
     //std::cout << "A * B = "; (C * D).print();
     //std::cout << "A *= B is "; (C *= D).print();
     std::cout << "A / B = "; (C / D).print();
     std::cout << "A /= B is "; (C /= D).print();
+    
+    std::cout << "reduce(A) = "; A.reduce(); A.print();
+
    
 
-    return 0;
+    //return 0;
 } 
